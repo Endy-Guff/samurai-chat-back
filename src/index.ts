e88ff4e198cca732f405e76e2a75660c4ebef0ad
Endy-Guff <andreymaranchak@yandex.ru>
2023-08-20 22:31:30 +0300

    add init server messages, add message logic

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
 import express from 'express'
 import http from 'http'
 import { Server } from "socket.io"
+import cors from 'cors'
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }});
+app.use(cors())
+const PORT = process.env.PORT || 3009
+
+const messages = [
+    {id: 'qwert1', message: 'Hi!', user: {id: 'ww1', name: 'Ivan'}},
+    {id: 'qwert2', message: 'WhatsUp', user: {id: 'ww2', name: 'Ignat'}}
+]
 
 app.get('/', (req, res) => {
     res.send('Hi!')
 });
 
 io.on('connection', (socket) => {
+    socket.on('socket-message-send', (message: string)=>{
+        messages.push({id: 'id234', message, user: {id: 'ww1', name: 'Ivan'}})
+        socket.emit('init-messages', messages)
+    })
+
+    socket.emit('init-messages', messages)
+
     console.log('a user connected');
 });
 
-server.listen(3009, () => {
-    console.log('listening on *:3009');
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });
